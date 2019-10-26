84686ed00fd85e091ecd839330d6e394c16469bd
Dmitri Kudrenko <darkdelphin@inbox.ru>
2019-10-26 18:40:48 +0300

    Task 709 To Lower Case

---
+++ src/main/java/task709/Solution.java
@@ -0,0 +1,11 @@
+package task709;
+
+/**
+ * Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ * */
+
+public class Solution {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
